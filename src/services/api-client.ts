9a752ed916dc6deea7f52aed63c3a61eaea74b5e
sabarish kumar <sabarish204@hotmail.com>
2025-06-18 21:06:27 +0530

    api client modified

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,21 @@
+// import axios from "axios";
+// export default axios.create({
+//   baseURL: "/api/",
+//   params: {
+//     key: "74a73354afd74100acc02d5f35b5a8ca"
+// }
+// });
+
+
+
+
 import axios from "axios";
 
+const isDev = import.meta.env.DEV;
+
 export default axios.create({
-  baseURL: "/api/",
+  baseURL: isDev ? "/api/" : "https://api.rawg.io/api/",
   params: {
-    key: "74a73354afd74100acc02d5f35b5a8ca"
-}
+    key: "74a73354afd74100acc02d5f35b5a8ca",
+  },
 });
